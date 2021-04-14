f38e779cceaeff161c4f6591641c43f2d780077f
arthur <arthur@vanloo.nu>
2021-04-14 20:19:12 +0200

    Lab 5 - Generate a utility lib

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { formatRating } from '@bg-hoard/store/util-formatters';
 import { getAllGames } from '../fake-api';
 
 @Component({
@@ -8,5 +9,6 @@ import { getAllGames } from '../fake-api';
 })
 export class AppComponent {
   title = 'Board Game Hoard';
+  formatRating = formatRating;
   games = getAllGames();
 }
---
+++ libs/store/util-formatters/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/store-util-formatters';
+// export * as formatRating from './lib/store-util-formatters.module';
