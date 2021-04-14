4cf035373e6d881b039203f606f979dd2b123fcc
arthur <arthur@vanloo.nu>
2021-04-14 19:47:46 +0200

    lab1

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { getAllGames } from '../fake-api';
 
 @Component({
-  selector: 'bg-hoard-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
