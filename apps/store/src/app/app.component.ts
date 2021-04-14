e11120c48fcc6757b4c7f39a7d1827e226da5f8a
arthur <arthur@vanloo.nu>
2021-04-14 21:25:25 +0200

    Lab 8 - Displaying a full game in the routed game-detail component

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,6 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
-import { getAllGames } from '../fake-api';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,8 @@ import { getAllGames } from '../fake-api';
 export class AppComponent {
   title = 'Board Game Hoard';
   formatRating = formatRating;
-  games = getAllGames();
+  // games = getAllGames();
+
+  constructor(private http: HttpClient) {}
+  games = this.http.get<any[]>('/api/games');
 }
