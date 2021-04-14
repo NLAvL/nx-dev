d2d0f708f732d8208b1f7faa204a72995ee48678
arthur <arthur@vanloo.nu>
2021-04-14 20:56:29 +0200

    api works

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,14 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('games')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
 
   @Get()
   getAllGames() {
