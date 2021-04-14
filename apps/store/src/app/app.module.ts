51b63af44b34d77e405dd20c204f35262834a271
arthur <arthur@vanloo.nu>
2021-04-14 20:05:49 +0200

    lab 4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    StoreUiSharedModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
   ],
   providers: [],
--- libs/store/ui-shared/src/lib/store-ui-shared.module.ts
+++ libs/store/ui-shared/src/lib/store-ui-shared.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    HeaderComponent
-  ],
-  exports: [
-    HeaderComponent
-  ],
+  imports: [CommonModule, MatToolbarModule],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
 })
 export class StoreUiSharedModule {}
