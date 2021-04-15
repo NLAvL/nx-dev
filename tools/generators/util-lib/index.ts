7bfe58de6bf25ca304a8bc49a657a421073402e6
arthur <arthur@vanloo.nu>
2021-04-15 19:34:00 +0200

    did not work like planned: nx workspace-generator util-lib notifications

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -5,6 +5,7 @@ export default async function (host: Tree, schema: any) {
   await libraryGenerator(host, {
     name: `util-${schema.name}`,
     directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`,
   });
   await formatFiles(host);
   return () => {
