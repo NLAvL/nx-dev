555e62fd4d2c2678557be9bf4b9bacfff2f6bbfb
arthur <arthur@vanloo.nu>
2021-04-15 21:59:58 +0200

    Lab 17 - Yes we can.... ✅

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('store');
+    expect(app.title).toEqual('Board Game Hoard');
   });
 
   it('should render title', () => {
