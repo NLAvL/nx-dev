c41cd8b0fcd9220ebd03d282e0283c1c10379c02
arthur <arthur@vanloo.nu>
2021-04-15 19:44:40 +0200

    14

--- tools/generators/sort-project-references/index.ts
+++ tools/generators/sort-project-references/index.ts
@@ -1,8 +1,15 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import {
+  formatFiles,
+  installPackagesTask,
+  Tree,
+  updateJson,
+} from '@nrwl/devkit';
 
-export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+export default async function (host: Tree) {
+  await updateJson(host, 'workspace.json', (workspaceJson) => {
+    workspaceJson.defaultProject = 'api';
+    return workspaceJson;
+  });
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
